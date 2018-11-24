e25e078b1d2c668ff926068baec6da9f929c9dcc
Dave Ankin <alexanderankin@gmail.com>
2018-11-24 09:01:41 -0500

    compressed and geojsons now

--- stateFeatureCollections.js
+++ stateFeatureCollections.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- *
+ * Script to generate single sldl/sldu GeoJSONs with all a state's regions.
  */
 
 var fs = require('fs');
@@ -20,27 +20,56 @@ var fiftys = states.filter(function (state) {
 
 console.log("Found", fiftys.length, "states to process.");
 
+function compressRegion(region) {
+  var g_ = region.geometry;
+  if (g_.type.toLowerCase() === 'polygon') {
+    g_.coordinates = g_.coordinates.map(function (polygon) {
+      polygon.map(function (point) {
+        point[0] = parseFloat(point[0].toFixed(3));
+        point[1] = parseFloat(point[1].toFixed(3));
+      });
+      return polygon.filter((el, i, a) => (i % 2 === 0));
+    });
+  } else if (g_.type.toLowerCase() === 'multipolygon') {
+    g_.coordinates = g_.coordinates.map(function (mpolygon) {
+      mpolygon.map(function (polygon) {
+        polygon.map(function (point) {
+          point[0] = parseFloat(point[0].toFixed(3));
+          point[1] = parseFloat(point[1].toFixed(3));
+        });
+      });
+      return mpolygon.filter((el, i, a) => (i % 2 === 0));
+    });
+  }
+  return region;
+}
+
+// console.log(compressRegion(JSON.parse(
+//   fs.readFileSync(pj(statesDir, 'AK', 'sldl', '16.geojson'), 'utf-8'))
+// ));
+
+function combineRegions(dir, regionNames) {
+  return regionNames.reduce(function (gJ, d) {
+    var district = JSON.parse(fs.readFileSync(pj(dir, d), 'utf-8'));
+    district = compressRegion(district);
+    return gJ.features.push(district), gJ;
+  }, { type: 'FeatureCollection', features: [] });
+}
 
 function reduceState(state) {
   console.log("Upper: Reading");
   var upperDir = pj(statesDir, state, 'sldu');
-  var upperDs = fs.readdirSync(upperDir);
-  var upperGeoJSON = upperDs.reduce(function (gJ, d) {
-    var district = JSON.parse(fs.readFileSync(pj(upperDir, d), 'utf-8'));
-    return gJ.features.push(district), gJ;
-  }, { type: 'FeatureCollection', features: [] });
+  var upperDs = fs.readdirSync(upperDir).filter(n => n !== 'sldu.geojson');
+  var upperGeoJSON = combineRegions(upperDir, upperDs);
   console.log("Upper: Writing");
-  fs.writeFileSync(pj(upperDir, 'sldu.json'), JSON.stringify(upperGeoJSON));
+  fs.writeFileSync(pj(upperDir, 'sldu.geojson'), JSON.stringify(upperGeoJSON));
   
   console.log("Lower: Reading");
   var lowerDir = pj(statesDir, state, 'sldl');
-  var lowerDs = fs.readdirSync(lowerDir);
-  var lowerGeoJSON = lowerDs.reduce(function (gJ, d) {
-    var district = JSON.parse(fs.readFileSync(pj(lowerDir, d), 'utf-8'));
-    return gJ.features.push(district), gJ;
-  }, { type: 'FeatureCollection', features: [] });
+  var lowerDs = fs.readdirSync(lowerDir).filter(n => n !== 'sldl.geojson');
+  var lowerGeoJSON = combineRegions(lowerDir, lowerDs);
   console.log("Lower: Writing");
-  fs.writeFileSync(pj(lowerDir, 'sldl.json'), JSON.stringify(lowerGeoJSON));
+  fs.writeFileSync(pj(lowerDir, 'sldl.geojson'), JSON.stringify(lowerGeoJSON));
 }
 
 fiftys.forEach(function (state) {
