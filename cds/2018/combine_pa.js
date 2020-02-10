02d0e39cedc414d5cb166be5c5bda29a256d3aba
David Ankin <daveankin@gmail.com>
2020-02-09 22:24:10 -0500

    compression

--- cds/2018/combine_pa.js
+++ cds/2018/combine_pa.js
@@ -5,9 +5,29 @@ var folders = fs.readdirSync('.').filter(f => f.startsWith('PA-'));
 
 var pa = { type: 'FeatureCollection', features: [] };
 
+function reviver(field, value) {
+  if (Array.isArray(value) && value.length > 100) {
+    var oldvalue = value;
+    value = [];
+    for (var j = 0, k = 0; j < oldvalue.length; j++) {
+      if ((j + 1) % 8 == 0)
+        continue;
+      value[k++] = oldvalue[j];
+    }
+  }
+
+  else if (Array.isArray(value) && value.length > 0 && !isNaN(value[0])) {
+    for (var i = 0; i < value.length; i++) {
+      value[i] = new Number(parseFloat(value[i]).toFixed(5));
+    }
+  }
+
+  return value;
+}
+
 for (var i = 0; i < folders.length; i++) {
   var file = path.join(__dirname, folders[i], 'shape.geojson');
-  var contents = JSON.parse(fs.readFileSync(file, 'utf8'));
+  var contents = JSON.parse(fs.readFileSync(file, 'utf8'), reviver);
   pa.features.push(contents);
 }
 
